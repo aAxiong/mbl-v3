623f0c21a5fe93af4ad9350ecc755a0080e19fa7
aAxiong <879628926@qq.com>
2018-12-17 18:00:46 +0800

    会员等级页面

--- page/mine/level_list/level_list.js
+++ page/mine/level_list/level_list.js
@@ -6,13 +6,59 @@ Page({
    */
   data: {
     snapName: '',
-    progress: 40
+    toView: 'toView0',
+    progress: 40,
+    levelDiscount: [{
+      level: '青铜',
+      discount: '无折扣'
+    }, {
+      level: '白银',
+      discount: '9.8折'
+    }, {
+      level: '黄金',
+      discount: '9.5折'
+    }, {
+      level: '白金',
+      discount: '9.3折'
+    }, {
+      level: '钻石',
+      discount: '9折'
+    }],
+    levelnow: 0,
+    levelTop: [{
+      level: '青铜',
+      starInt: 0,
+      endInt: 100
+    }, {
+      level: '白银',
+      starInt: 0,
+      endInt: 100
+    }, {
+      level: '黄金',
+      starInt: 0,
+      endInt: 100
+    }, {
+      level: '白金',
+      starInt: 0,
+      endInt: 100
+    }, {
+      level: '钻石',
+      starInt: 0,
+      endInt: 100
+    }],
+    level_tiao: 0,
+    level_k: -1
+
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    wx.showLoading({
+      title: '加载中...',
+    })
     this.getUserInfoAuth()
   },
   //用户是否授权
@@ -38,7 +84,7 @@ Page({
         this.setData({
           userInfo: res.userInfo
         })
-
+        wx.hideLoading();
       }
     })
   },
@@ -59,7 +105,14 @@ Page({
       }
     })
   },
-
+  changeLevel: function(e) {
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      level_tiao: index,
+      level_k: index,
+      toView: 'toView' + index
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
